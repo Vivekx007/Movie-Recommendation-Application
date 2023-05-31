f110adcbfdfa18f21996cead972357e38e6fe5e7
Vivek <vivek.network9525@gmail.com>
2023-06-01 01:09:02 +0530

    final comit

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+// const cors = require("cors");
+const port = 8000;
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const moviesRoute = require("./routes/movies");
+const listsRoute = require("./routes/lists");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection is successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+// app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/movies", moviesRoute);
+app.use("/api/lists", listsRoute);
+
+app.listen(port, () => {
+  console.log(`backend server is running on port ${port}`);
+});
